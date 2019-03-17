ab4c03019650c414c3abad166b4f52204863817a
Frederick Ofosu-Darko <fofosudarko@gmail.com>
2019-03-17 01:18:02 +0000

    add inputs used to calculate bmi and alert user when form is submitted

--- src/components/bmi-calculator/CalculateBMI.js
+++ src/components/bmi-calculator/CalculateBMI.js
@@ -1,20 +1,85 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { View, Alert, TextInput, Button, StyleSheet } from 'react-native';
 
-const CalculateBMI = props => (
-  <View style={styles.calculator}>
-    <Text>Calculate BMI</Text>
-  </View>
-);
+class CalculateBMI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: null,
+      height: null
+    };
+
+    this.weightPlaceholder = 'Enter your weight';
+    this.heightPlaceholder = 'Enter your height';
+  }
+
+  onWeightInput = weight => {
+    this.setState({ weight });
+  };
+
+  onHeightInput = height => {
+    this.setState({ height });
+  };
+
+  onGetBMI = () => {
+    let { height, weight } = this.state;
+    let alertMessage = `
+      Your height: ${height},
+      Your weight: ${weight}
+    `;
+
+    Alert.alert('BMI', alertMessage);
+  };
+
+  render() {
+    return (
+      <View style={styles.calculator}>
+        <View style={styles.formContainer}>
+          <TextInput
+            style={styles.weight}
+            onChangeText={this.onWeightInput}
+            placeholder={this.weightPlaceholder}
+          />
+          <TextInput
+            style={styles.height}
+            onChangeText={this.onHeightInput}
+            placeholder={this.heightPlaceholder}
+          />
+          <Button title="Get BMI" color="green" onPress={this.onGetBMI} />
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   calculator: {
     flex: 1,
     width: '100%',
     height: '45%',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     padding: 20
-  }
+  },
+  formContainer: {
+    flex: 1,
+    height: '60%',
+    justifyContent: 'space-between'
+  },
+  weight: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  height: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  submitContainer: {
+    flex: 3,
+    height: '30%'
+  },
+  submit: {}
 });
 
 export default CalculateBMI;
