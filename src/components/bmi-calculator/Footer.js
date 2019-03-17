dfef733a3cf207b3b329d2b0953dbf0e0d0e8624
Frederick Ofosu-Darko <fofosudarko@gmail.com>
2019-03-17 01:16:29 +0000

    add copyright and current year

--- src/components/bmi-calculator/Footer.js
+++ src/components/bmi-calculator/Footer.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import utils from 'app/src/utils/utils';
+
 const Footer = props => (
   <View style={styles.footer}>
-    <Text>Footer</Text>
+    <Text style={styles.text}>&copy;&nbsp;{utils.getYear()}</Text>
   </View>
 );
 
 const styles = StyleSheet.create({
   footer: {
+    flex: 1,
     height: '7%',
-    backgroundColor: 'green'
+    backgroundColor: 'green',
+    justifyContent: 'center'
+  },
+  text: {
+    flex: 1,
+    width: '50%',
+    height: '100%',
+    alignSelf: 'center',
+    alignContent: 'flex-end',
+    padding: 5,
+    fontSize: 25,
+    color: 'white',
+    textAlign: 'center'
   }
 });
 
