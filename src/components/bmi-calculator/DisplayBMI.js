d553b021b87a74b907f9630e15418b1f9d2e57e5
Frederick Ofosu-Darko <fofosudarko@gmail.com>
2019-03-17 15:53:23 +0000

    implement bmi display to user

--- src/components/bmi-calculator/DisplayBMI.js
+++ src/components/bmi-calculator/DisplayBMI.js
@@ -1,18 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const DisplayBMI = props => (
-  <View style={styles.display}>
-    <Text>Display BMI</Text>
+import BMIService from 'app/src/services/services';
+
+class DisplayBMI extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let { bmi } = this.props;
+
+    return (
+      <View style={styles.displayContainer}>
+        {!bmi ? <Placeholder /> : <Display bmi={this.props.bmi} />}
+      </View>
+    );
+  }
+}
+
+const Placeholder = props => (
+  <View style={styles.innerDisplay}>
+    <Text style={styles.placeholder}>Body Fat</Text>
+    <Text style={styles.placeholder}>OK?</Text>
+  </View>
+);
+
+const Display = ({ bmi }) => (
+  <View style={styles.innerDisplay}>
+    <Text style={styles.display}>BMI: {bmi}</Text>
+    <Text style={styles.display}>
+      You are {BMIService.getHealthStatus(bmi)}
+    </Text>
   </View>
 );
 
 const styles = StyleSheet.create({
-  display: {
-    flex: 1,
+  displayContainer: {
     width: '100%',
-    height: '45%',
+    height: '50%',
     padding: 20
+  },
+  innerDisplay: {
+    backgroundColor: 'snow',
+    borderColor: 'green',
+    borderWidth: 3,
+    borderRadius: 10,
+    height: '100%',
+    width: '100%',
+    padding: 20,
+    justifyContent: 'space-around'
+  },
+  placeholder: {
+    fontSize: 70,
+    textAlign: 'center',
+    color: 'green'
+  },
+  display: {
+    fontSize: 50,
+    textAlign: 'center',
+    color: 'green'
   }
 });
 
