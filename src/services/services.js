e086bce0d3259a3ebaa3164b7d28ae76eddf60cc
Frederick Ofosu-Darko <fofosudarko@gmail.com>
2019-03-17 15:54:32 +0000

    add services.js

---
+++ src/services/services.js
@@ -0,0 +1,24 @@
+// services.js
+
+const calculateBMI = (weight = 1, height = 1) => {
+  return (weight / Math.pow(height, 2)).toFixed(4);
+};
+
+const getHealthStatus = bmi => {
+  let healthStatus = '';
+
+  if (bmi >= 25) {
+    healthStatus = 'overweight';
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    healthStatus = 'healthy';
+  } else {
+    healthStatus = 'underweight';
+  }
+
+  return healthStatus;
+};
+
+export default {
+  calculateBMI,
+  getHealthStatus
+};
