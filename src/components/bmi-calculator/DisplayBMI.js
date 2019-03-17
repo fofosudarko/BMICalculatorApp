9d759b83e1e0f0cb499446654a79b8c83f0e0ed0
Frederick Ofosu-Darko <fofosudarko@gmail.com>
2019-03-17 19:48:41 +0000

    style health status display based on bmi

--- src/components/bmi-calculator/DisplayBMI.js
+++ src/components/bmi-calculator/DisplayBMI.js
@@ -19,21 +19,63 @@ class DisplayBMI extends Component {
   }
 }
 
-const Placeholder = props => (
-  <View style={styles.innerDisplay}>
-    <Text style={styles.placeholder}>Body Fat</Text>
-    <Text style={styles.placeholder}>OK?</Text>
-  </View>
-);
+const Placeholder = props => {
+  const style = {
+    backgroundColor: 'snow',
+    borderColor: 'green'
+  };
 
-const Display = ({ bmi }) => (
-  <View style={styles.innerDisplay}>
-    <Text style={styles.display}>BMI: {bmi}</Text>
-    <Text style={styles.display}>
-      You are {BMIService.getHealthStatus(bmi)}
-    </Text>
-  </View>
-);
+  const placeholderStyle = StyleSheet.flatten([styles.innerDisplay, style]);
+
+  return (
+    <View style={placeholderStyle}>
+      <Text style={styles.placeholder}>Body Fat</Text>
+      <Text style={styles.placeholder}>OK?</Text>
+    </View>
+  );
+};
+
+const Display = ({ bmi }) => {
+  let healthStatus = BMIService.getHealthStatus(bmi);
+  let displayStyle = StyleSheet.flatten([
+    styles.innerDisplay,
+    getHealthStatusStyle(healthStatus)
+  ]);
+
+  return (
+    <View style={displayStyle}>
+      <Text style={styles.display}>BMI: {bmi}</Text>
+      <Text style={styles.display}>You are {healthStatus}!</Text>
+    </View>
+  );
+};
+
+const getHealthStatusStyle = healthStatus => {
+  let style = {};
+
+  switch (healthStatus) {
+    case 'overweight':
+      style = {
+        borderColor: 'midnightblue',
+        backgroundColor: 'midnightblue'
+      };
+      break;
+    case 'underweight':
+      style = {
+        borderColor: 'red',
+        backgroundColor: 'red'
+      };
+      break;
+    case 'healthy':
+      style = {
+        borderColor: 'green',
+        backgroundColor: 'green'
+      };
+      break;
+  }
+
+  return style;
+};
 
 const styles = StyleSheet.create({
   displayContainer: {
@@ -42,8 +84,6 @@ const styles = StyleSheet.create({
     padding: 20
   },
   innerDisplay: {
-    backgroundColor: 'snow',
-    borderColor: 'green',
     borderWidth: 3,
     borderRadius: 10,
     height: '100%',
@@ -59,7 +99,7 @@ const styles = StyleSheet.create({
   display: {
     fontSize: 50,
     textAlign: 'center',
-    color: 'green'
+    color: 'white'
   }
 });
 
