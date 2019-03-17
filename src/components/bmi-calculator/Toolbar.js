7509fd33244c95a21acc0b2f3fe5f62f5bd5de39
Frederick Ofosu-Darko <fofosudarko@gmail.com>
2019-03-17 15:54:22 +0000

    remove flex property

--- src/components/bmi-calculator/Toolbar.js
+++ src/components/bmi-calculator/Toolbar.js
@@ -9,13 +9,11 @@ const Toolbar = props => (
 
 const styles = StyleSheet.create({
   toolbar: {
-    flex: 1,
     width: '100%',
     height: '100%',
     padding: 10
   },
   title: {
-    flex: 1,
     width: '40%',
     height: '100%',
     color: 'white',
